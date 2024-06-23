8a8329cb4d22e4898cda93b028956a92895b53e0
LauraaTanase <zaraflaura@gmail.com>
2024-06-23 18:39:52 -0500

    scripts.js modified

--- scripts.js
+++ scripts.js
@@ -1,7 +1,136 @@
 const products = [
-  { id: 1, name: "Product 1", price: 10 },
-  { id: 2, name: "Product 2", price: 20 },
-  { id: 3, name: "Product 3", price: 30 },
+  {
+    id: 1,
+    name: "Banane",
+    description:
+      "Bananele sunt un fruct tropical extrem de sănătos, nu numai delicios. Deoarece conțin mai mulți nutrienți esențiali, bananele oferă beneficii pentru digestie, sănătatea inimii și pierderea în greutate. Sunt o gustare convenabilă, îndrăgită atât de adulți, cât și de copii.",
+    price: 10,
+    stock: 15,
+    image:
+      "C:UserszarafOneDriveDesktopIT SCHOOL WEB DEVELOPMENTVSCmini-shopassets\banana.jpg",
+    sku: "SK001",
+  },
+  {
+    id: 2,
+    name: "Mere",
+    description:
+      "Merele sunt fructe crocante și suculente, disponibile într-o varietate de culori, inclusiv roșu, verde și galben. Sunt o sursă excelentă de fibre și vitamine, în special vitamina C.",
+    price: 10,
+    stock: 15,
+    image:
+      "C:UserszarafOneDriveDesktopIT SCHOOL WEB DEVELOPMENTVSCmini-shopassetsmere.jpg",
+    sku: "SK002",
+  },
+  {
+    id: 3,
+    name: "Pere",
+    description:
+      "Perele sunt fructe dulci și zemoase. Ele sunt bogate în fibre și antioxidanți, fiind perfecte atât pentru consumul proaspăt, cât și pentru gătit.",
+    price: 10,
+    stock: 15,
+    image:
+      "C:UserszarafOneDriveDesktopIT SCHOOL WEB DEVELOPMENTVSCmini-shopassetspere.jpg",
+    sku: "SK003",
+  },
+  {
+    id: 4,
+    name: "Fructul dragonului",
+    description:
+      " Fructul dragonului este un fruct exotic cu coajă roz sau galbenă și pulpă albă sau roșie, punctată cu semințe negre. Este bogat în vitamina C, fibre și antioxidanți.",
+    price: 10,
+    stock: 15,
+    image:
+      "C:UserszarafOneDriveDesktopIT SCHOOL WEB DEVELOPMENTVSCmini-shopassets\fructulDragonului",
+    sku: "SK004",
+  },
+  {
+    id: 5,
+    name: "Piersici",
+    description:
+      "Piersicile sunt fructe suculente, care pot varia în culoare de la alb la galben. Sunt cunoscute pentru aroma lor dulce și conținutul ridicat de vitamine A și C.",
+    price: 10,
+    stock: 15,
+    image:
+      "C:UserszarafOneDriveDesktopIT SCHOOL WEB DEVELOPMENTVSCmini-shopassetspiersici.jpg",
+    sku: "SK005",
+  },
+  {
+    id: 6,
+    name: "Struguri",
+    description:
+      "Strugurii sunt fructe mici, suculente, care cresc în ciorchini. Disponibili în varietăți de culori precum verde, roșu și negru, strugurii sunt bogați în antioxidanți și vitamine.",
+    price: 10,
+    stock: 15,
+    image:
+      "C:UserszarafOneDriveDesktopIT SCHOOL WEB DEVELOPMENTVSCmini-shopassetsstruguri.jpg",
+    sku: "SK006",
+  },
+  {
+    id: 7,
+    name: "Nectarine",
+    description:
+      "Nectarinele sunt similare piersicilor, dar au o coajă netedă și un gust dulce-acrișor. Sunt o sursă bună de vitamine A și C și antioxidanți.",
+    price: 10,
+    stock: 15,
+    image:
+      "C:UserszarafOneDriveDesktopIT SCHOOL WEB DEVELOPMENTVSCmini-shopassets\nectarine.jpg",
+    sku: "SK007",
+  },
+  {
+    id: 8,
+    name: "Mandarine",
+    description:
+      " Mandarinele sunt citrice mici și dulci, cu coajă subțire și ușor de curățat. Ele sunt bogate în vitamina C și flavonoizi, fiind o gustare perfectă pentru imunitate.",
+    price: 10,
+    stock: 15,
+    image:
+      "C:UserszarafOneDriveDesktopIT SCHOOL WEB DEVELOPMENTVSCmini-shopassetsmandarine.jpg",
+    sku: "SK008",
+  },
+  {
+    id: 9,
+    name: "Portocale",
+    description:
+      " Portocalele sunt fructe citrice mari și suculente, cunoscute pentru gustul lor dulce și acidulat. Sunt o sursă excelentă de vitamina C și fibre.",
+    price: 10,
+    stock: 15,
+    image:
+      "C:UserszarafOneDriveDesktopIT SCHOOL WEB DEVELOPMENTVSCmini-shopassetsportocale.jpg",
+    sku: "SK009",
+  },
+  {
+    id: 10,
+    name: "Lime",
+    description:
+      "Lime-ul este un fruct citric mic, verde, cu un gust acrișor intens. Este folosit frecvent pentru a da aromă alimentelor și băuturilor, fiind bogat în vitamina C.",
+    price: 10,
+    stock: 15,
+    image:
+      "C:UserszarafOneDriveDesktopIT SCHOOL WEB DEVELOPMENTVSCmini-shopassetslime.jpg",
+    sku: "SK0010",
+  },
+  {
+    id: 11,
+    name: "Mango",
+    description:
+      "Mango-ul este un fruct tropical cu pulpă galben-portocalie, dulce și suculentă. Este bogat în vitaminele A și C, fibre și antioxidanți.",
+    price: 10,
+    stock: 15,
+    image:
+      "C:UserszarafOneDriveDesktopIT SCHOOL WEB DEVELOPMENTVSCmini-shopassetsmango.jpg",
+    sku: "SK0011",
+  },
+  {
+    id: 12,
+    name: "Ananas",
+    description:
+      "Ananasul este un fruct tropical mare, cu o coajă aspră și pulpă dulce-acrișoară. Este o sursă excelentă de vitamina C, mangan și bromelaină, o enzimă benefică pentru digestie.",
+    price: 10,
+    stock: 15,
+    image:
+      "C:UserszarafOneDriveDesktopIT SCHOOL WEB DEVELOPMENTVSCmini-shopassetsananas.jpg",
+    sku: "SK0012",
+  },
 ];
 
 let cart = [];
@@ -62,31 +191,31 @@ function renderCart() {
       // sa devina un string, stringul fiind cel asteptat in .innerHTML (ceva de genul: "<li>test</li><li>test</li><li>test</li>")
       .join("");
 
-//  TODO: implementeaza ce sa faca remove from cart
+  //  TODO: implementeaza ce sa faca remove from cart
   document.querySelectorAll(".remove-from-cart").forEach((button) => {
-      button.addEventListener("click", (event) => {
-    // Obținem id-ul produsului din atributul data-id al butonului
-    const productId = parseInt(event.target.getAttribute("data-id"));
+    button.addEventListener("click", (event) => {
+      // Obținem id-ul produsului din atributul data-id al butonului
+      const productId = parseInt(event.target.getAttribute("data-id"));
 
-    // Apelăm funcția removeFromCart cu productId pentru a elimina produsul din coș
-    removeFromCart(productId);
+      // Apelăm funcția removeFromCart cu productId pentru a elimina produsul din coș
+      removeFromCart(productId);
+    });
   });
-});
-  }
+}
 
 function addToCart(productId) {
   const product = products.find((p) => p.id === productId);
 
   // TODO: "impinge" produsul in lista de cart
   // asta trebuie sa faci tu :)
-cart.push({
-  id:product.id,
-  name:product.name,
-  price:product.price
-});
+  cart.push({
+    id: product.id,
+    name: product.name,
+    price: product.price,
+  });
 
- // Actualizăm afișarea numărului de produse în coș
- updateCartCount(); 
+  // Actualizăm afișarea numărului de produse în coș
+  updateCartCount();
 
   // ca sa afisez actualizat - practic fac override la ce am deja in innerHTML
   renderCart();
@@ -97,8 +226,8 @@ function removeFromCart(productId) {
   // obtin un array fara ce am pasat in input
   cart = cart.filter((item) => item.id !== productId);
 
-    // Actualizăm afișarea numărului de produse în coș
-  updateCartCount()
+  // Actualizăm afișarea numărului de produse în coș
+  updateCartCount();
 
   // update la datele afisate
   renderCart();
@@ -108,38 +237,36 @@ function updateCartCount() {
   cartCountElement.textContent = cart.length;
 }
 
-  // TODO: conditioneaza un alert message daca nu ai continut
-  // HINT:
-  // if (
-  //   // cartul nu are continut
-  // ) {
-  //   alert("Your cart is empty!");
-  //   return;
-  // }
-  function checkout() {
-    if (cart.length === 0) {
-      alert("Your cart is empty");
-      return;
-    }
-  
+// TODO: conditioneaza un alert message daca nu ai continut
+// HINT:
+// if (
+//   // cartul nu are continut
+// ) {
+//   alert("Your cart is empty!");
+//   return;
+// }
+function checkout() {
+  if (cart.length === 0) {
+    alert("Your cart is empty");
+    return;
+  }
+
   // TODO: calculeaza totalul cartului
 
-
-  const total = cart.reduce((acc, item) => acc + item.price, 0)
+  const total = cart.reduce((acc, item) => acc + item.price, 0);
 
   // afiseaza mesajul
-  alert(`Your total is $${total}. Thank you for your purchase!`) 
-  
+  alert(`Your total is $${total}. Thank you for your purchase!`);
+
   // acum ca a dat checkout si "a cumparat"
   // golim cartul pentru alte cumparaturi :)
-  
-  cart = [] // Clear the cart
+
+  cart = []; // Clear the cart
 
   // si facem iar update
   renderCart();
 }
 
-
-document.getElementById("checkout-btn").addEventListener("click", checkout) 
+document.getElementById("checkout-btn").addEventListener("click", checkout);
 
 renderProducts();
